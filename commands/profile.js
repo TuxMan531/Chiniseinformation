9119ae42c2c48281859562ff02665525796de5d4
TuxMan531 <jaxf2005@gmail.com>
2023-09-13 15:42:53 -0500

    `epic commit message`

--- commands/profile.js
+++ commands/profile.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder, AttachmentBuilder } = require('discord.js');
-const { createCanvas } = require('@napi-rs/canvas');
+const { createCanvas, loadImage } = require('@napi-rs/canvas');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -9,9 +9,17 @@ module.exports = {
 
         const canvas = createCanvas(700, 250);
         const context = canvas.getContext('2d');
+        // load and draw background
+        const background = await loadImage('./wallpaper.png');
+        context.drawImage(background, 0, 0, canvas.width, canvas.height);
+        
+        // Optional: Additional drawing or text
+        // context.font = '10px sans-serif';
+        // context.fillText('asdasdasda', 100, 100);
+
         const attachment = new AttachmentBuilder(canvas.toBuffer('image/png'), { name: 'profile-image.png' });
-        context.font = '10px sans_sherif'
-        context.fillText('asdasdasda',100,100)
+       // context.font = '10px sans_sherif'
+     //   context.fillText('asdasdasda',100,100)
 
         console.warn(interaction.user)
 		await interaction.reply(
