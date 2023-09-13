91f0aadfc0795aba9a745963243fd7439d853bd4
TuxMan531 <jaxf2005@gmail.com>
2023-09-13 15:45:14 -0500

    yeah

--- commands/profile.js
+++ commands/profile.js
@@ -7,21 +7,21 @@ module.exports = {
 		.setDescription('gets users profile'),
 	async execute(interaction) {
 
-        const canvas = createCanvas(700, 250);
+        const canvas = createCanvas(1920, 1080);
         const context = canvas.getContext('2d');
         // load and draw background
-        const background = await loadImage('./wallpaper.png');
+        const background = await loadImage('./background.jpg');
         context.drawImage(background, 0, 0, canvas.width, canvas.height);
-        
-        // Optional: Additional drawing or text
-        // context.font = '10px sans-serif';
-        // context.fillText('asdasdasda', 100, 100);
+        // the text
+        context.font = '100px Arial';
+        context.fillStyle = 'white';
+        context.fillText(`Username: ${interaction.user.username}`, 100, 100);
+        context.fillText(`Display Name: ${interaction.user.displayName}`, 100, 200);
+
+        const pfp = await loadImage('https://cdn.discordapp.com/avatars/309429229458685962/4e2af354f31be18c2048600ac5762e6b.webp?size=1024&width=0&height=281', 500, 500);
 
         const attachment = new AttachmentBuilder(canvas.toBuffer('image/png'), { name: 'profile-image.png' });
-       // context.font = '10px sans_sherif'
-     //   context.fillText('asdasdasda',100,100)
-
-        console.warn(interaction.user)
+        console.log(interaction.user)
 		await interaction.reply(
             { files: [attachment] }
 
