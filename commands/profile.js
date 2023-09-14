9a099294db9437472377c1e6cfc6efdaf7ff0262
TuxMan531 <jaxf2005@gmail.com>
2023-09-13 22:13:52 -0500

    minecraft font and userpfp and nickname var

--- commands/profile.js
+++ commands/profile.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder, AttachmentBuilder } = require('discord.js');
-const { createCanvas, loadImage,} = require('@napi-rs/canvas');
+const { createCanvas, loadImage, GlobalFonts } = require('@napi-rs/canvas');
 const { request } = require('undici');
+const { join } = require('path');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -10,22 +11,28 @@ module.exports = {
 
         const canvas = createCanvas(1920, 1080);
         const context = canvas.getContext('2d');
+        // load fonts
+        GlobalFonts.registerFromPath(join(__dirname, 'Minecraft.ttf'), 'Minecraft')
+        console.log(GlobalFonts.families)
+
         // load and draw background
         const background = await loadImage('./background.jpg');
         context.drawImage(background, 0, 0, canvas.width, canvas.height);
         // the text
-        context.font = '100px Arial';
+        context.font = '100px Minecraft, Arial';
         context.fillStyle = 'white';
         context.fillText(`Username: ${interaction.user.username}`, 100, 100);
         context.fillText(`Display Name: ${interaction.user.displayName}`, 100, 200);
+        context.fillText(`Nickname: ${interaction.member.nickname}`, 100, 300);
+        
         //pfp image
         //const pfp = await loadImage('./pfpf.png');
         //context.drawImage(pfp, (1920 - 556), 0,);
-        const { body } = await request(interaction.user.displayAvatarURL({ extension: 'jpg' }));
+        const { body } = await request(interaction.user.displayAvatarURL({ extension: 'jpg' , size:512}));
 	const avatar = await loadImage(await body.arrayBuffer());
-        context.drawImage(avatar, 0, 0,);
+        context.drawImage(avatar, (1920 - 512), 0,);
 
-        const attachment = new AttachmentBuilder(canvas.toBuffer('image/png'), { name: 'profile-image.png' });
+        const attachment = new AttachmentBuilder(canvas.toBuffer('image/png'), { name: 'profile-image.png'});
         console.log(interaction.user)
 		await interaction.reply(
             { files: [attachment] }
